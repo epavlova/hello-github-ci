b3ec336b623bb30c01adb304243aa5ac3a9ef8b6
Elitsa Pavlova <elitsa.pavlova@ft.com>
2020-01-06 13:35:30 +0200

    Add intentialnal lint error

--- main.go
+++ main.go
@@ -1,8 +1,12 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+)
 
 func main() {
 	fmt.Println("Hello GitHub Actions!")
 	fmt.Println("Why there is no way to manually run GitHub workflow?")
+	json.Marshal("test")
 }
