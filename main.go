2756aa07c9cee1924f325955184a490fe636c9c8
Elitsa Pavlova <elitsa.pavlova@ft.com>
2020-01-14 13:11:01 +0200

    f

--- main.go
+++ main.go
@@ -1,12 +1,7 @@
 package main
 
-import (
-	"encoding/json"
-	"fmt"
-)
+import "fmt"
 
 func main() {
 	fmt.Println("Hello GitHub Actions!")
-	fmt.Println("Why there is no way to manually run GitHub workflow?")
-	json.Marshal("test")
 }
